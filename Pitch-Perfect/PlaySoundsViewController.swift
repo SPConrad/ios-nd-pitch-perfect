139036208637ae9cb1dc8051aedf69d362b8a434
Sean Conrad <spconrad@gmail.com>
2018-05-28 19:37:59 -0400

    ready to submit

---
+++ Pitch-Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,80 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch-Perfect
+//
+//  Created by Sean Conrad on 4/29/18.
+//  Copyright © 2018 Sean Conrad. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var allButtons = [UIButton]()
+    
+    var recordedAudioURL: URL!
+    
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
+    
+    let labels = ["Slooooow", "FAST!", "ALVIN!!!", "Darth", "EchooOOOoo", "Reverrrrb"]
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAudio()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func playSoundForButton(_ sender: UIButton){
+        print("Play sound button pressed")
+        
+        switch(ButtonType(rawValue: sender.tag)!){
+            case .slow:
+                playSound(rate: 0.5)
+            case .fast:
+                playSound(rate: 1.5)
+            case .chipmunk:
+                playSound(pitch: 1000)
+            case .vader:
+                playSound(pitch: -1000)
+            case .echo:
+                playSound(echo: true)
+            case .reverb:
+                playSound(reverb: true)
+        }
+        self.title = labels[sender.tag]
+        configureUI(.playing)
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject){
+        print("Stop audio button pressed")
+        configureUI(.notPlaying)
+        self.title = ""
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ Pitch-Perfect/RecordSoundsViewController.swift
@@ -0,0 +1,105 @@
+//
+//  RecordSoundViewController.swift
+//  Pitch-Perfect
+//
+//  Created by Sean Conrad on 4/22/18.
+//  Copyright © 2018 Sean Conrad. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+// a Class can only inherit from one superclass, but it can conform to as many other protocols as you want
+class RecordSoundViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var startStopRecordingButton: UIButton!
+    
+    let startRecordImage = UIImage(named: "Record")
+    let stopRecordingImage = UIImage(named: "Stop")
+    
+    let startRecordingString = "Tap to Begin Recording"
+    let stopRecordingString = "Tap to Stop Recording"
+    
+    var audioRecorder: AVAudioRecorder!
+    
+    enum recordButtonState: Int {
+        case record = 0, stop
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        startStopRecordingButton.imageView?.image = startRecordImage
+        recordLabel.text = startRecordingString
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func startStopButton(_ sender: UIButton){
+        if recordButtonState(rawValue: sender.tag)! == .record {
+            startAudioRecording()
+            sender.tag = 1
+        } else {
+            stopAudioRecording()
+            sender.tag = 0
+        }
+    }
+    
+    func startAudioRecording() {
+        recordLabel.text = stopRecordingString
+        startStopRecordingButton.setImage(stopRecordingImage, for: .normal)
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+    
+    func showAlert(){
+        let ac = UIAlertController(title: "Recording unsuccesful", message: "Please try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: nil))
+        present(ac, animated: true)
+    }
+    
+    func stopAudioRecording() {
+        recordLabel.text = startRecordingString
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+        startStopRecordingButton.setImage(startRecordImage, for: .normal)
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+       
+        if flag{
+            performSegue(withIdentifier: "stopRecordingSegue", sender: audioRecorder.url)
+        } else {
+            showAlert()
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "stopRecordingSegue" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
+    
+}
+
